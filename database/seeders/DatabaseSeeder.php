d1fd266ea21ffccd6cd03f272530172c86b52d60
Taufiq <htaufiq225@gmail.com>
2024-02-22 16:32:10 +0800

    memperbaiki database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,5 +32,126 @@ class DatabaseSeeder extends Seeder
         Role::create([
             'role_name' => 'Admin',
         ]);
+
+        User::create([
+            'username' => 'taufiq',
+            'email' => 'taufiq@gmail.com',
+            'password' => bcrypt('matematika'),
+            'role_id' => 1
+        ]);
+
+        Mahasiswa::create([
+            'user_id' => 1,
+            'statusTA' => 'Belum Mengajukan'
+        ]);
+
+        User::create([
+            'username' => 'dosen',
+            'email' => 'dosen@gmail.com',
+            'password' => bcrypt('matematika'),
+            'role_id' => 2
+        ]);
+
+        Dosen::create([
+            'user_id' => 2
+        ]);
+
+        User::create([
+            'username' => 'kajur',
+            'email' => 'kajur@gmail.com',
+            'password' => bcrypt('matematika'),
+            'role_id' => 3
+        ]);
+
+        Kajur::create([
+            'user_id' => 3
+        ]);
+
+        User::create([
+            'username' => 'sekjur',
+            'email' => 'sekjur@gmail.com',
+            'password' => bcrypt('matematika'),
+            'role_id' => 4
+        ]);
+
+        Sekjur::create([
+            'user_id' => 4
+        ]);
+
+        User::create([
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('matematika'),
+            'role_id' => 5
+        ]);
+
+        Admin::create([
+            'user_id' => 5
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Teknik Pengembangan Wilayah dan Kota
+            '
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Teknik Informatika'
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Teknik Arsitektur'
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Biologi'
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Kimia'
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Fisika'
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Matematika'
+        ]);
+
+        Jurusan::create([
+            'jurusan' => 'Ilmu Peternakan'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Syariah dan Hukum'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Tarbiyah dan Keguruan'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Ushuluddin Filsafat dan Politik'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Adab dan Humaniora'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Dakwah dan Komunikasi'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Sains dan Teknologi'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Kedokteran dan Ilmu Kesehatan'
+        ]);
+
+        Fakultas::create([
+            'fakultas' => 'Ekonomi dan Bisnis Islam'
+        ]);
     }
 }
