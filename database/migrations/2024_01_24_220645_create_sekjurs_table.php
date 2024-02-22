f96c67828ebb67670843e7c9bec6dfc9adb4271c
Taufiq <htaufiq225@gmail.com>
2024-02-22 16:37:01 +0800

    memperbaiki file database seeder

--- database/migrations/2024_01_24_220645_create_sekjurs_table.php
+++ database/migrations/2024_01_24_220645_create_sekjurs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sekjurs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('nama')->nullable();
+            $table->string('nip')->nullable();
             $table->timestamps();
         });
     }
