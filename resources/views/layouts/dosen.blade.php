64aea134b17a96b2e2aef968905e3c36d0dd5e31
Taufiq <htaufiq225@gmail.com>
2024-03-17 09:00:27 +0800

    memperbaharui layout

--- resources/views/layouts/dosen.blade.php
+++ resources/views/layouts/dosen.blade.php
@@ -54,34 +54,6 @@
 	            />
 	            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SITA</span>
 	          </a>
-	          <form action="#" method="GET" class="hidden md:block md:pl-2">
-	            <label for="topbar-search" class="sr-only">Search</label>
-	            <div class="relative md:w-64 md:w-96">
-	              <div
-	                class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
-	              >
-	                <svg
-	                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
-	                  fill="currentColor"
-	                  viewBox="0 0 20 20"
-	                  xmlns="http://www.w3.org/2000/svg"
-	                >
-	                  <path
-	                    fill-rule="evenodd"
-	                    clip-rule="evenodd"
-	                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-	                  ></path>
-	                </svg>
-	              </div>
-	              <input
-	                type="text"
-	                name="email"
-	                id="topbar-search"
-	                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-	                placeholder="Search"
-	              />
-	            </div>
-	          </form>
 	        </div>
 	        <div class="flex items-center lg:order-2">
 	          <button
@@ -90,10 +62,6 @@
 	            aria-controls="drawer-navigation"
 	            class="p-2 mr-1 text-gray-500 rounded-lg md:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
 	          >
-	            <span class="sr-only">Toggle search</span>
-	            <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-	              <path clip-rule="evenodd" fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
-	            </svg>
 	          <button type="button"
 	            class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
 	            id="user-menu-button"
@@ -142,28 +110,6 @@
 	            >
 	              <li>
 	                <a
-	                  href="#"
-	                  class="flex items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-	                  ><svg
-	                    class="mr-2 w-5 h-5 text-gray-400"
-	                    fill="currentColor"
-	                    viewBox="0 0 20 20"
-	                    xmlns="http://www.w3.org/2000/svg"
-	                  >
-	                    <path
-	                      d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"
-	                    ></path>
-	                  </svg>
-	                  Collections</a
-	                >
-	              </li>
-	            </ul>
-	            <ul
-	              class="py-1 text-gray-700 dark:text-gray-300"
-	              aria-labelledby="dropdown"
-	            >
-	              <li>
-	                <a
 	                  href=" {{ route('logout') }} "
 	                  class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 	                  >Sign out</a
@@ -181,34 +127,6 @@
 	      aria-label="Sidenav"
 	      id="drawer-navigation">
 	      <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
-	        <form action="#" method="GET" class="md:hidden mb-2">
-	          <label for="sidebar-search" class="sr-only">Search</label>
-	          <div class="relative">
-	            <div
-	              class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
-	            >
-	              <svg
-	                class="w-5 h-5 text-gray-500 dark:text-gray-400"
-	                fill="currentColor"
-	                viewBox="0 0 20 20"
-	                xmlns="http://www.w3.org/2000/svg"
-	              >
-	                <path
-	                  fill-rule="evenodd"
-	                  clip-rule="evenodd"
-	                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-	                ></path>
-	              </svg>
-	            </div>
-	            <input
-	              type="text"
-	              name="search"
-	              id="sidebar-search"
-	              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-	              placeholder="Search"
-	            />
-	          </div>
-	        </form>
 	        <ul class="space-y-2">
 	          <li>
 	            <a
