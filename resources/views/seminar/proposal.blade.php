20f16c10a7761357f70b94c6cd283acd876e5983
Taufiq <htaufiq225@gmail.com>
2024-03-19 11:12:41 +0800

    fix bug validation in seminar layout

--- resources/views/seminar/proposal.blade.php
+++ resources/views/seminar/proposal.blade.php
@@ -18,7 +18,7 @@
 	  </button>
 	</div>
 @endif
-@if($status === false)
+@if($status === true)
 	<div class="w-full h-full" >
 		 <section class="bg-white dark:bg-gray-900">
 		    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
