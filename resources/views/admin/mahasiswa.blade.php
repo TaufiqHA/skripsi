1f282ae8f1a1d45953bd3607c3c8f1dbab75fd3a
Taufiq <htaufiq225@gmail.com>
2024-03-20 15:59:18 +0800

    fix bug layout tambah angkatan

--- resources/views/admin/mahasiswa.blade.php
+++ resources/views/admin/mahasiswa.blade.php
@@ -10,6 +10,7 @@
 			  <span class="sr-only">Info</span>
 			  <div class="ms-3 text-sm font-medium">
 			    {{ session()->get('success') }}
+			  </div>
 			  <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-3" aria-label="Close">
 			    <span class="sr-only">Close</span>
 			    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
