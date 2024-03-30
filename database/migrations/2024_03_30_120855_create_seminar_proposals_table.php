22e77f64652ea1f1912c0f5c91339852de061ed8
Taufiq <htaufiq225@gmail.com>
2024-03-30 20:12:05 +0800

    membuat model seminar

---
+++ database/migrations/2024_03_30_120855_create_seminar_proposals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seminar_proposals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mahasiswa_id');
+            $table->date('tanggal');
+            $table->string('tempat');
+            $table->string('dosen_penguji1');
+            $table->string('dosen_penguji2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seminar_proposals');
+    }
+};
