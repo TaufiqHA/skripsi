9ec86e9b512dc30dc6cac3606dbe658bc02db9be
Taufiq <htaufiq225@gmail.com>
2024-04-01 15:00:15 +0800

    menambahkan fitur event pada halaman dashboard

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -53,8 +53,122 @@
 			        	@endif
 			            
 			        </div>
-			        <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="statistics" role="tabpanel" aria-labelledby="statistics-tab">
-			            <!--  -->
+			        <div class="hidden p-2 bg-white rounded-lg md:p-2 dark:bg-gray-800" id="statistics" role="tabpanel" aria-labelledby="statistics-tab">
+						<div id="controls-carousel" class="relative w-full" data-carousel="static">
+						    <!-- Carousel wrapper -->
+						    <div class="relative h-64 overflow-y-auto rounded-lg">
+						         <!-- Item 1 -->
+						        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+						            <div id="accordion-collapse" data-accordion="collapse">
+									  <h2 id="accordion-collapse-heading-1">
+									    <div class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+									      <span>Seminar proposal</span>
+									    </div>
+									  </h2>
+									  <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
+									    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+											<ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+											    <li>
+											        Tempat : Jurusan matematika
+											    </li>
+											    <li>
+											        Tanggal/Hari : Senin, tanggal
+											    </li>
+											    <li>
+											        Penguji 1 : dosen TI
+											    </li>
+											    <li>
+											        Penguji 2 : dosen SI
+											    </li>
+											    <li>
+											        Download sk pembimbing
+											    </li>
+											</ul>
+									    </div>
+									  </div>
+									</div>
+						        </div>
+						        <!-- Item 2 -->
+						        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+						            <div id="accordion-collapse" data-accordion="collapse">
+									  <h2 id="accordion-collapse-heading-1">
+									    <div class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+									      <span>Seminar hasil</span>
+									    </div>
+									  </h2>
+									  <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
+									    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+											<ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+											    <li>
+											        Tempat : Jurusan matematika
+											    </li>
+											    <li>
+											        Tanggal/Hari : Senin, tanggal
+											    </li>
+											    <li>
+											        Penguji 1 : dosen TI
+											    </li>
+											    <li>
+											        Penguji 2 : dosen SI
+											    </li>
+											    <li>
+											        Download sk pembimbing
+											    </li>
+											</ul>
+									    </div>
+									  </div>
+									</div>
+						        </div>
+						        <!-- Item 3 -->
+						        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+						            <div id="accordion-collapse" data-accordion="collapse">
+									  <h2 id="accordion-collapse-heading-1">
+									    <div class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+									      <span>Ujian munaqasyah</span>
+									    </div>
+									  </h2>
+									  <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
+									    <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+											<ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+											    <li>
+											        Tempat : Jurusan matematika
+											    </li>
+											    <li>
+											        Tanggal/Hari : Senin, tanggal
+											    </li>
+											    <li>
+											        Penguji 1 : dosen TI
+											    </li>
+											    <li>
+											        Penguji 2 : dosen SI
+											    </li>
+											    <li>
+											        Download sk pembimbing
+											    </li>
+											</ul>
+									    </div>
+									  </div>
+									</div>
+						        </div>
+						    </div>
+						    <!-- Slider controls -->
+						    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+						        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+						            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+						                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+						            </svg>
+						            <span class="sr-only">Previous</span>
+						        </span>
+						    </button>
+						    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+						        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+						            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+						                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+						            </svg>
+						            <span class="sr-only">Next</span>
+						        </span>
+						    </button>
+						</div>
 			        </div>
 			    </div>
 			</div>
